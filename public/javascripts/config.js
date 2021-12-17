3af197dfc0dc472d0d5bdc6dfc08cd059206dd53
cathulhu <ellbee11@outlook.com>
2021-12-16 22:55:21 -0800

    initial site download functions

---
+++ public/javascripts/config.js
@@ -0,0 +1,475 @@
+const fs = require('fs');
+const http = require('http');
+const axios = require('axios');
+let exec = require('child_process').exec, child;
+const xml2js = require('xml2js');
+const { XMLParser } = require('fast-xml-parser');
+const util = require('util');
+const { $0 } = require('prettier');
+const { merge } = require('../../routes');
+
+
+
+
+
+
+
+
+
+
+
+function loadConfig() {
+
+    let hardCodedSites = {}
+
+    fs.readFile('../../config_files/hardCodedSites.json', 'utf8' , (err, data) => {
+      if (err) {
+        console.error(err)
+        return
+      }
+    
+      hardCodedSites = JSON.parse(data)
+      console.log(hardCodedSites)
+    })
+
+}
+
+
+
+
+
+
+
+
+async function buildSites() {
+
+  let rawSites = await downloadSites()
+  let processedResults = await processSites(rawSites)
+  let postMergeResults = await mergeAllDuplicates(processedResults)
+  let finalSitesObject = await buildLookupTables(postMergeResults)
+  console.log(finalSitesObject)
+
+}
+
+
+
+
+
+function tidyUp () {
+  
+}
+
+
+
+
+
+
+async function downloadSites() {
+
+  // *TODO* Note don't leave this in   vvvvvvvv      eventually we DO want to secure the site
+  process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+
+
+  
+  let result = await axios.get('https://dune-cric.cern.ch/api/dune/vofeed//list/', {headers: { "Accept":"application/xml" }})
+
+  console.log("\n")
+  // console.log(result.data)
+
+  //NOTE, if there is collision between attribute names set attributeNamePrefix to "_@" and use [""] notation to access them (versus . notation)
+  let parser = new XMLParser({ignoreAttributes: false, attributeNamePrefix : ""});
+  let objResult = parser.parse(result.data)
+
+
+  // let siteObject =  await processSites(objResult.root.atp_site)
+  let siteObject = objResult.root.atp_site
+
+  return siteObject
+
+ }
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ function processSites(serializedSites) {
+
+  let uniqueSiteIDs = generateUniqueIDarray(serializedSites.length)
+
+
+  let results = []
+  results.sites = []
+  for (x in serializedSites) {
+
+    //build the final version of the JS object from this data
+    let siteObject = {}
+
+    siteObject.assignedID = uniqueSiteIDs[x]
+
+    siteObject.latitude = serializedSites[x].latitude
+    siteObject.longitude = serializedSites[x].longitude
+
+    siteObject.names = extractNames(serializedSites[x])
+
+    // console.log("\n")
+    siteObject.mergeTarget = findPotentialDuplicateSites(siteObject, results)
+
+    results.sites.push(siteObject)
+    // console.log(siteObject)
+  }
+
+  return results
+  // mergeDuplicates(results)
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ function generateUniqueIDarray(count) {
+
+  ids=[]
+
+  for (let i=0; i < count; i++) {
+    let newID = Math.floor(100000 + Math.random() * 900000)
+
+    while ( ids.includes(newID) ){
+      newID = Math.floor(100000 + Math.random() * 900000)
+    }
+
+    ids.push(newID)
+  }
+
+  return ids
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ function extractNames (individualSite) {
+
+  // let debugswitch = false
+
+  //this regex selects all the charactesr before _ in name, IE country codes
+  let countryCodeRegex = ".*_";
+  //then do theString.replace(regexResult, "") to clear out contry code
+
+  //dune prefix followed by two character country code remover IE DUNE_US_BNL_SDCC -> BNL_SDCC   or    DUNE_FR_CCIN2P3_DISK -> CCIN2P3_DISK
+
+  // console.log("raw", individualSite)
+
+  names = [];
+  //for instance here we add BR_CBPF and CBPF to the names list that this record might be under
+  names.push(individualSite.name)
+  names.push(individualSite.name.replace(new RegExp(countryCodeRegex), ""))
+
+  // console.log(individualSite.group)
+
+  //harvest names from the group section
+  for (y in individualSite.group) {
+    if ( !individualSite.group[y].name.toLowerCase().includes("tier") ) {  //ignore names like "Tier-3"
+      if (!names.includes(individualSite.group[y].name)){ //if name isn't already in last, add it
+        names.push(individualSite.group[y].name)
+        names.push(individualSite.group[y].name.replace(new RegExp(countryCodeRegex), ""))
+
+        //above we push the regular name, and then we try to push it with country code removed IE BR_CBPF and CBPF
+      }
+    }
+
+    // if (debugswitch) {console.log(names)}
+  }
+
+  // console.log("before", names)
+
+  //do a filter to remove duplicates from this entry before we return
+  names = [...new Set(names)]
+
+  // console.log("after SET", names, "\n\n")
+
+  return names
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ function findPotentialDuplicateSites(individialSite, allSites) {
+
+  let currentNamesLength = individialSite.names.length
+  let siteCount = allSites.sites.length
+  // let firstFound = false
+
+  let mergeCandidateList = []
+  let mergeParentList = []
+  // console.log("\n\n\n", allSites.sites)
+
+
+  for (let i=0; i < currentNamesLength; i++) {
+    let currentName = individialSite.names[i]
+
+    for (let j=1; j < siteCount; j++) {
+
+      // console.log("here")
+
+      let candidateSite = allSites.sites[j] 
+      for (let k=0; k < candidateSite.names.length; k++) {
+
+        if (candidateSite.names[k] === currentName) {
+          // console.log("overlap found")
+          //here we've detected overlap between names IE multiple sites for which they both have UCHICHAGO, I interprit this to mean these are the same site so they'll be merged for the purpose of this program
+          
+          // console.log(candidateSite.names[k], " versus ", currentName)
+          // if (!firstFound) {mergeCandidateList.push(individialSite.assignedID)}
+          // firstFound = true
+          mergeCandidateList.push(candidateSite.assignedID)
+          mergeParentList.push(individialSite.assignedID)
+          break
+        }
+      }
+
+    }
+  }
+
+  if (mergeCandidateList > 0) {
+    console.log("collision found in site names between these IDs: ", mergeCandidateList, " and ", mergeParentList)
+
+  }
+
+
+
+  if (mergeCandidateList.length > 0) {
+    return mergeCandidateList
+  } else return false
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ function mergeAllDuplicates (sitesObject) {
+
+  let sites = sitesObject.sites
+  // console.log(sitesObject.sites)
+
+  for (x in sites) {
+    let currentSite = sites[x]
+
+    // console.log(currentSite.mergeTarget)
+    // console.log(sitesObject.sites.length)
+
+    if (currentSite.mergeTarget !== false) {
+      for (y in currentSite.mergeTarget) {
+
+        let currentMergeTargetID = currentSite.mergeTarget[y]
+        let mergeTargetArrayIndex;
+        let mergeTargetSite = {}
+
+        for (z in sites) {
+          if (sites[z].assignedID === currentMergeTargetID) {
+            mergeTargetSite = sites[z]
+            mergeTargetArrayIndex = z
+          }
+        }
+
+        if (currentMergeTargetID===undefined) {
+          console.log("ERROR, merge target ID not found!")
+        }
+
+        console.log("merge flag found on site: ", currentSite.assignedID, "@ array index: ", x ,"   for target: ", currentMergeTargetID , "@ array index: ", mergeTargetArrayIndex)
+
+        //now do the merging
+        
+        if ( Math.abs(currentSite.longitude) < 1 && Math.abs(currentSite.longitude) < 1) {  // 1-,-1 to 1,1 is just ocean, I checked, so there's no way this can be legit
+          currentSite.longitude = mergeTargetSite.longitude
+          currentSite.latitude = mergeTargetSite.latitude   //the the parent has bad coordinates then worst case we get 0,0 again and maybe get better ones
+        }  
+
+        currentSite.names = currentSite.names.concat(mergeTargetSite.names)
+        //this ^ likely will cause duplicates so we will clean those up by making a unique set now...
+
+        currentSite.names = [...new Set(currentSite.names)]
+
+        currentSite.mergeTarget = false   //set to false after we've already done the copying
+
+        //now to delete the child we've absorbed the details of
+
+        sitesObject.sites.splice(mergeTargetArrayIndex, 1)
+      }
+    }
+  }
+  // console.log(sitesObject.sites)
+  return sitesObject
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ function buildLookupTables(allSites) {
+  let IDlookupMap = new Map();
+  let nameLookupMap = new Map();
+
+
+
+  for (x in allSites.sites) {
+
+    let siteObject = allSites.sites[x]
+
+    IDlookupMap.set(siteObject.assignedID, allSites.sites.indexOf(siteObject))
+    nameLookupMap.set(siteObject.names, allSites.sites.indexOf(siteObject))
+  }
+
+  // console.log(allSites, "\n\n")
+
+  allSites.IDlookupMap = IDlookupMap
+  allSites.nameLookupMap = nameLookupMap
+
+  return allSites
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+buildSites()
+
+
+
+
+
+
+
+
+// loadConfig()
+// downloadSites()
+
